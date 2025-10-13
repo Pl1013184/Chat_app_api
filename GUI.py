ce8b1ce0880fb878e446286024e7db0134ca7772
Pl1013184 <pl1013184@ahschool.com>
2025-10-12 20:36:54 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,17 +2,33 @@ import tkinter as tk
 import login
 import requests
 username = login.LogIN()[True]
-def post_message(message):
-    requests.post('http://127.0.0.1:5000/main/chat', data={'data': message})
+
+def post_message(message,username):
+    requests.post(f'http://127.0.0.1:5000/main/', data={'data': message,"username":username})
+
+
+def get_messages(username):
+    response =requests.get(f'http://127.0.0.1:5000/main/',data={'username':username})
+    #print(response.text)
+    messages.set(requests.get(f'http://127.0.0.1:5000/main/',data={'username':username}).text)
+    chat_window.after(1000,lambda: get_messages(username))
 chat_window = tk.Tk()
 message=tk.StringVar()
+messages=tk.StringVar()
 chat_window.title('Chat')
-chat_window.resizable(width=False, height=False)
-chat_window.geometry('600x150')
+chat_window.resizable(width=False, height=True)
+chat_window.geometry('600x300')
 user_label = tk.Label(chat_window,text=f'{username}:')
 user_label.place(x=25,y=50)
 username_entry = tk.Entry(chat_window, width=20,textvariable=message)
 username_entry.place(x=50,y=50)
-mesage_send=tk.Button(chat_window,text='Send',command=lambda: post_message(message.get()))
+chat_frame = tk.Frame(chat_window,relief='raised',borderwidth=5)
+chat_frame.place(x=50,y=100)
+chats_window=tk.Label(chat_frame,textvariable=messages)
+chats_window.pack(side='top')
+#accesschat=tk.Button(chat_frame,text='Access Chat',command=lambda: get_messages(username,messages))
+#accesschat.pack()
+mesage_send=tk.Button(chat_window,text='Send',command=lambda: post_message(message.get(),username))
 mesage_send.place(x=50,y=80)
+get_messages(username)
 chat_window.mainloop()
