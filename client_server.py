cac2719807c7a3bc356392ca7310d87b32267373
Pl1013184 <pl1013184@ahschool.com>
2025-10-12 20:42:58 -0400

    Update client_server.py

--- client_server.py
+++ client_server.py
@@ -1,7 +1,1 @@
-#test
-import requests
-message=input("Enter your message: ")
-response = requests.post('http://127.0.0.1:5000/main/chat',data={'data':message})
-print(response.text)
-x=requests.get('http://127.0.0.1:5000/main/chat')
-print(x.text)
+Import GUI
