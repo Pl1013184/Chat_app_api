c8c8130d01576ba8691b8091b2c8102b0adb46fc
ezram <pl1013184@ahschool.com>
2025-10-25 15:49:20 -0400

    Chat_app

--- test_webserver.py
+++ test_webserver.py
@@ -2,5 +2,9 @@ from flask import Flask, request, jsonify,render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
+@app.route('/<user>')
+def chat():
+@app.route('/<user>/<room>')
+def chats(username):
     return render_template('main.html')
 app.run()
